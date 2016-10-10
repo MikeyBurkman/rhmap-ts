5bebd7983cf1a1a8782b6d21f006780ce718a30d
MikeyBurkman <michael.burkman@gmail.com>
2016-10-10 14:10:22 -0500

    tests oh yah

--- lib/application.ts
+++ lib/application.ts
@@ -50,7 +50,7 @@ app.use(function(err: Error, req: express.Request, res: express.Response, next: 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
-const port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
+const port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8100;
 const host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
   console.log('App started at: ', new Date(), 'on port: ', port);
---
+++ lib/foo.test.ts
@@ -0,0 +1,6 @@
+
+describe(__filename, () => {
+    it('Should run a test!', () => {
+        console.log('Running the test!');
+    });
+});
