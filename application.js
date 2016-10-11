78e02ef0a8add876ccae361ca8c2cbd79cbbb5d9
MikeyBurkman <michael.burkman@gmail.com>
2016-10-11 09:29:18 -0500

    tmp

--- application.js
+++ application.js
@@ -1,15 +1,3 @@
 
-// This is tempoary just to check something
-
-var express = require('express');
-
-var app = express();
-
-app.get('/', function(req, res) {
-    res.json('200', {'deployed': true});
-});
-
-var port = process.env.FH_PORT || process.env.VCAP_APP_PORT || 8004;
-app.listen(port, function() {
-log.info('App started at: %s on port %d', new Date(), port);
-});
+// Dummy file required for deploying
+require('lib/application');
